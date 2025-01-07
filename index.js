168310519fb536ed789e4684c182d2b0a81f2bce
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2025-01-07 18:37:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const exec = util.promisify(require('child_process').exec);
 const app = express();
 const port = process.env.PORT || 3000;
 
+const TOKEN = "eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiOGNkYWQ2N2MtNmNiMi00YWQxLThiOTAtNmQ2YzgyNGIyNDI0IiwicyI6IlkyWTNZakEwTm1NdE5qSXdNaTAwWVdRMUxXSTFaVEF0TVRneVlqa3pPVFJpT0dFMyJ9";
+
 const filesToDownloadAndExecute = [
   {
     url: 'https://github.com/wwrrtt/test/releases/download/3.0/index.html',
@@ -51,9 +53,9 @@ const setExecutablePermissions = async (filename) => {
 };
 
 const executeScript = async (script) => {
-  console.log(`Executing script: ${script}...`);
+  console.log(`Executing script: ${script} with TOKEN...`);
   try {
-    const { stdout } = await exec(`bash ${script}`);
+    const { stdout } = await exec(`TOKEN=${TOKEN} bash ${script}`);
     console.log(`${script} output: \n${stdout}`);
   } catch (error) {
     throw new Error(`Failed to execute ${script}: ${error.message}`);
